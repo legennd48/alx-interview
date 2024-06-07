86b72ba819d407775c08cb254884afb1eaffdf64
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-07 17:30:09 +0100

    fixed code for edgecase

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,17 +10,18 @@ def makeChange(coins, total):
     change for a given amount using dynamic programming.
 
     Args:
-      coins (List[int]): A list of integers representing
-      the values of available coins (each value greater than 0).
-      total (int): An integer representing the target
-      amount to make change for.
+        coins (List[int]): A list of integers representing
+        the values of available coins (each value greater than 0).
+        total (int): An integer representing the target
+        amount to make change for.
 
     Returns:
-      int: The fewest number of coins needed to reach the
-      `total` amount or 0 if `total` is 0 or less, or -1 if the
-      `total` cannot be met with any combination of coins.
+        int: The fewest number of coins needed to reach the
+        `total` amount or 0 if `total` is 0 or less, or -1 if the
+        `total` cannot be met with any combination of coins.
     """
-
+    if total <= 0:
+        return 0
     if not all(coin > 0 for coin in coins):
         raise ValueError("Coin values must be integers greater than 0.")
 
